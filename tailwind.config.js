3bf258b0dfa2e23e84a94c368c9ecba7bbaa27b4
darekaze <darekaze@gmail.com>
2020-08-02 11:27:09 +0800

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// Theme doc: https://tailwindcss.com/docs/theme/
+// Supporing plugins: https://twin-docs.netlify.app/plugin-support
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  plugins: [],
+}
