1ed9bad110c82482d0a3c32e09b2f2d57fcbbbe6
darekaze <darekaze@gmail.com>
2020-08-02 18:48:24 +0800

    add: temporary seo

---
+++ next-seo.config.ts
@@ -0,0 +1,20 @@
+import type { DefaultSeoProps } from 'next-seo'
+
+const SEO: DefaultSeoProps = {
+  title: 'Darwin Jahja | Full Stack Software Engineer',
+  description:
+    'Darwin Jahja (a.k.a @darekaze) is a Full Stack Software Engineer based in HK & Macao who specializes in creating and developing exceptional, high-quality websites and applications.',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://darekaze.dev/',
+    site_name: 'Darwin Jahja | Full Stack Software Engineer',
+  },
+  twitter: {
+    site: '@_DaRekaze',
+    cardType: 'summary_large_image',
+  },
+  additionalMetaTags: [{ name: 'author', content: 'Darwin Jahja' }],
+}
+
+export default SEO
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,19 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
 import React from 'react'
+import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 import GlobalStyle from '@/layouts/GlobalStyle'
 import BaseLayout from '@/layouts/BaseLayout'
+import SEO from '@/next-seo.config'
 
 import 'tailwindcss/dist/base.min.css' // eslint-disable-line
-
-// TODO: add default seo here
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <GlobalStyle />
+      <DefaultSeo {...SEO} />
       <BaseLayout>
         <Component {...pageProps} />
       </BaseLayout>
